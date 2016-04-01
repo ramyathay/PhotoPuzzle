178e35e98053d7badd472da5e144c33dcbdec6c6
ramyathay <ramyatha.yugendernath@gmail.com>
2016-04-01 13:28:28 -0700

    Modified the lokk of the app

---
+++ PhotoPuzzle/ExtraCode.swift
@@ -0,0 +1,22 @@
+//
+//  ExtraCode.swift
+//  PhotoPuzzle
+//
+//  Created by Ramyatha Yugendernath on 3/9/16.
+//  Copyright © 2016 Ramyatha Yugendernath. All rights reserved.
+//
+
+import Foundation
+
+
+var allImageNamesArray:[String] = ["ironMan.jpg","captainAmerica.jpeg","hulk.jpg","thor.jpg","nickFury.jpg","loki.jpg","cb.jpg","blackWidow.jpg"
+]
+let imageFullNameDict = ["ironMan.jpg" : "The Iron Man - A self-described genius, billionaire, playboy, and philanthropist with an electromechanical suit of armor of his own invention.",
+    "captainAmerica.jpeg": "Captain America - A World War II veteran who was enhanced to the peak of human physicality by an experimental serum and frozen in suspended animation before waking up in the modern world.",
+    "hulk.jpg" : "The Hulk - A genius scientist who, because of exposure to gamma radiation, transforms into a monster when enraged or agitated.",
+    "thor.jpg": "Thor - The crown prince of Asgard, based on the Norse mythological deity of the same name.",
+    "nickFury.jpg": "Nick Fury - The director of S.H.I.E.L.D.,coordinating the 'Avengers Initiative'.",
+    "loki.jpg": "Loki - Thor's adoptive brother and nemesis, based on the Norse mythological deity of the same name.",
+    "cb.jpg": "Hawkeye - A master archer working as an agent for S.H.I.E.L.D.",
+    "blackWidow.jpg" : "A highly trained spy working for S.H.I.E.L.D."
+]
--- PhotoPuzzle/PhotoPuzzleViewController.swift
+++ PhotoPuzzle/PhotoPuzzleViewController.swift
@@ -13,17 +13,6 @@ class PhotoPuzzleViewController: UIViewController, UIGestureRecognizerDelegate {
     var previousUserGameCount: String = ""
     var imageNameDescription: String = ""
     //Holds names of all the images
-    var allImageNamesArray:[String] = ["ironMan.jpg","captainAmerica.jpeg","hulk.jpg","thor.jpg","nickFury.jpg","loki.jpg","cb.jpg","blackWidow.jpg"
-    ]
-    let imageFullNameDict = ["ironMan.jpg" : "The Iron Man - A self-described genius, billionaire, playboy, and philanthropist with an electromechanical suit of armor of his own invention.",
-        "captainAmerica.jpeg": "Captain America - A World War II veteran who was enhanced to the peak of human physicality by an experimental serum and frozen in suspended animation before waking up in the modern world.",
-        "hulk.jpg" : "The Hulk - A genius scientist who, because of exposure to gamma radiation, transforms into a monster when enraged or agitated.",
-        "thor.jpg": "Thor - The crown prince of Asgard, based on the Norse mythological deity of the same name.",
-        "nickFury.jpg": "Nick Fury - The director of S.H.I.E.L.D.,coordinating the 'Avengers Initiative'.",
-        "loki.jpg": "Loki - Thor's adoptive brother and nemesis, based on the Norse mythological deity of the same name.",
-        "cb.jpg": "Hawkeye - A master archer working as an agent for S.H.I.E.L.D.",
-        "blackWidow.jpg" : "A highly trained spy working for S.H.I.E.L.D."
-    ]
     var croppedImagesArray: [UIImage] = []   //Holds the original Image cropped Instances
     var shuffledImagesDict = [Int: Int]()   //Associates the image tag with the image in croppedImagesArray. This is to know which image resides in which image tag
     var handleSwipedImagesArray: [UIImage] = []   //For handling swiped Images position so that croppedImages Array remains altered and will be useful for checking against game end status
@@ -35,24 +24,34 @@ class PhotoPuzzleViewController: UIViewController, UIGestureRecognizerDelegate {
     override func viewDidLoad() {
         super.viewDidLoad()
         
-        if previousUserGameCount != "" {
-            imageNameLabel.hidden = false
-            imageNameLabel.text = "The Previous User's game play time was \(previousUserGameCount) secs"
-        }
-        else {
-            imageNameLabel.hidden = true
-        }
+        runTimerLabel.textAlignment = .Center
+        let screenSize: CGRect = UIScreen.mainScreen().bounds
+        let screenScale: CGFloat = UIScreen.mainScreen().scale
+        let imageSize = CGSize(width: screenSize.width * screenScale, height: screenSize.height * screenScale)
+        
+//        if previousUserGameCount != "" {
+//            imageNameLabel.hidden = false
+//            imageNameLabel.text = "The Previous User's game play time was \(previousUserGameCount) secs"
+//        }
+//        else {
+//            imageNameLabel.hidden = true
+//        }
         
         //Generating a random image from collection of images
-        let selectImageName = Int(arc4random_uniform(UInt32(allImageNamesArray.count)))
+        //let selectImageName = Int(arc4random_uniform(UInt32(allImageNamesArray.count)))
         
-        self.view.backgroundColor = UIColor.blackColor()
+        self.view.backgroundColor = UIColor.lightGrayColor()
         //let actualImage = UIImage(named: allImageNamesArray[selectImageName])
         
-        let url = NSURL(string: "https://source.unsplash.com/category/nature")
+        let url = NSURL(string: "https://source.unsplash.com/random")
         let data = NSData(contentsOfURL: url!)
         let actualImage = UIImage(data: data!)
-        //imageNameDescription = imageFullNameDict[allImageNamesArray[selectImageName]]!
+        UIGraphicsBeginImageContext(imageSize)
+        actualImage?.drawInRect((CGRect(origin: CGPointZero, size: imageSize)))
+        let scaledImage = UIGraphicsGetImageFromCurrentImageContext()
+        
+        print("Image size is",actualImage?.size.height,actualImage?.size.width)
+
         imageNameDescription = "Congratulations you have completed the puzzle"
         
         var randomNums = [0,1,2,3,4,5,6,7,8,9,10,11,12,13,14,15]
@@ -60,13 +59,13 @@ class PhotoPuzzleViewController: UIViewController, UIGestureRecognizerDelegate {
             var w:Int , h: Int
             for h = 0 ; h < 4 ; h++ {
                 for w = 0 ; w < 4 ; w++ {
-                    print("value of w and h is \(w), \(h)")
-                    let imgWidth: CGFloat = actualImage!.size.width/4;
-                    let imgHeight: CGFloat  = actualImage!.size.height/4;
-                    let imgFrame: CGRect = CGRectMake(CGFloat((actualImage!.size.width / 4) * CGFloat(w)), CGFloat((actualImage!.size.height / 4) * CGFloat(h) ), imgWidth, imgHeight)
-                    let imgInRect: CGImageRef = CGImageCreateWithImageInRect(actualImage!.CGImage, imgFrame)!
+                    let imgWidth: CGFloat = scaledImage!.size.width/4;
+                    let imgHeight: CGFloat  = scaledImage!.size.height/4;
+                    let imgFrame: CGRect = CGRectMake(CGFloat((scaledImage!.size.width / 4) * CGFloat(w)), CGFloat((scaledImage!.size.height / 4) * CGFloat(h) ), imgWidth, imgHeight)
+                    let imgInRect: CGImageRef = CGImageCreateWithImageInRect(scaledImage!.CGImage, imgFrame)!
                     let croppedImage: UIImage = UIImage(CGImage: imgInRect)
                     croppedImagesArray.append(croppedImage)
+                    print("Per image size is",scaledImage.size.width, scaledImage.size.height)
                 }
             }
         
@@ -76,14 +75,15 @@ class PhotoPuzzleViewController: UIViewController, UIGestureRecognizerDelegate {
                 let randomNumber = Int(arc4random_uniform(UInt32(randomNums.count)))
             
                 let randomImageNumber = randomNums[randomNumber]
-                 print("Random number is \(randomImageNumber)")
                 randomNums.removeAtIndex(randomNumber)
             
                 if let img = self.view.viewWithTag(k + 1) as! UIImageView! {
                     img.image = croppedImagesArray[randomImageNumber]
                     img.layer.borderWidth = 1
                     img.layer.borderColor = UIColor.whiteColor().CGColor
+                    img.contentMode = .ScaleToFill
                     shuffledImagesDict[k + 1] = randomImageNumber
+                    print("Cropped image",img.frame.height,img.frame.width,img.image?.size.height,img.image?.size.width)
                     
                     let directions: [UISwipeGestureRecognizerDirection] = [.Right, .Left, .Up, .Down]
                     for direction in directions {
@@ -168,7 +168,7 @@ class PhotoPuzzleViewController: UIViewController, UIGestureRecognizerDelegate {
         }
         if isGameOver {
             // create the alert
-            let alert = UIAlertController(title: "THE AVENGERS", message: imageNameDescription, preferredStyle: UIAlertControllerStyle.Alert)
+            let alert = UIAlertController(title: "The Photo Puzzle", message: imageNameDescription, preferredStyle: UIAlertControllerStyle.Alert)
             //adding Custom animations to the UIAlertController
             let subview = alert.view.subviews.first! as UIView
             let alertContentView = subview.subviews.first! as UIView
@@ -188,24 +188,51 @@ class PhotoPuzzleViewController: UIViewController, UIGestureRecognizerDelegate {
     func handleSwipe(sender: UISwipeGestureRecognizer) {
        //Current Image
         let img = self.view.viewWithTag(sender.view!.tag) as! UIImageView!
-        
-        
+
         //For Right Swipe
         if sender.direction.rawValue == 1 {
-            img.image = handleSwipedImagesArray[shuffledImagesDict[sender.view!.tag + 1]!]
+            img.image = self.handleSwipedImagesArray[self.shuffledImagesDict[sender.view!.tag + 1]!]
+            //Adding Animations to the Swiped Image
+//            UIView.animateWithDuration(0.6, delay: 0, options: .CurveEaseOut, animations: {
+//                img.alpha = 0.1
+//                img.image = self.handleSwipedImagesArray[self.shuffledImagesDict[sender.view!.tag + 1]!]
+//                }, completion: { (finished: Bool) -> Void in
+//                    UIView.animateWithDuration(0.06, animations: { () -> Void in
+//                        img.alpha = 1.0
+//                    })
+//            })
+            
             //Changing Swapped Image to Swiped Image
             let swapImg = self.view.viewWithTag(sender.view!.tag + 1) as! UIImageView!
-            swapImg.image = handleSwipedImagesArray[shuffledImagesDict[sender.view!.tag]!]
-            
+            //Adding Animations to the Swiped Image
+            UIView.animateWithDuration(0.6, delay: 0, options: .CurveEaseOut, animations: {
+                swapImg.alpha = 0.1
+                swapImg.image = self.handleSwipedImagesArray[self.shuffledImagesDict[sender.view!.tag]!]
+                }, completion: { (finished: Bool) -> Void in
+                    UIView.animateWithDuration(0.06, animations: { () -> Void in
+                        swapImg.alpha = 1.0
+                    })
+            })
+            //swapImg.image = self.handleSwipedImagesArray[self.shuffledImagesDict[sender.view!.tag]!]
+        
+          
             (handleSwipedImagesArray[shuffledImagesDict[sender.view!.tag]!],handleSwipedImagesArray[shuffledImagesDict[sender.view!.tag + 1]!]) = (handleSwipedImagesArray[shuffledImagesDict[sender.view!.tag + 1]!] , handleSwipedImagesArray[shuffledImagesDict[sender.view!.tag]!])
-            
             print("Right swipe")
+            
         } //For Left swipe
         else if sender.direction.rawValue == 2 {
             img.image = handleSwipedImagesArray[shuffledImagesDict[sender.view!.tag - 1]!]
             
             let swapImg = self.view.viewWithTag(sender.view!.tag - 1) as! UIImageView!
-            swapImg.image = handleSwipedImagesArray[shuffledImagesDict[sender.view!.tag]!]
+            UIView.animateWithDuration(0.6, delay: 0, options: .CurveEaseOut, animations: {
+                swapImg.alpha = 0.1
+                swapImg.image = self.handleSwipedImagesArray[self.shuffledImagesDict[sender.view!.tag]!]
+                }, completion: { (finished: Bool) -> Void in
+                    UIView.animateWithDuration(0.06, animations: { () -> Void in
+                        swapImg.alpha = 1.0
+                    })
+            })
+            
             
             (handleSwipedImagesArray[shuffledImagesDict[sender.view!.tag]!],handleSwipedImagesArray[shuffledImagesDict[sender.view!.tag - 1]!]) = (handleSwipedImagesArray[shuffledImagesDict[sender.view!.tag - 1]!] , handleSwipedImagesArray[shuffledImagesDict[sender.view!.tag]!])
             
@@ -215,7 +242,14 @@ class PhotoPuzzleViewController: UIViewController, UIGestureRecognizerDelegate {
             img.image = handleSwipedImagesArray[shuffledImagesDict[sender.view!.tag - 4]!]
             
             let swapImg = self.view.viewWithTag(sender.view!.tag - 4) as! UIImageView!
-            swapImg.image = handleSwipedImagesArray[shuffledImagesDict[sender.view!.tag]!]
+            UIView.animateWithDuration(0.6, delay: 0, options: .CurveEaseOut, animations: {
+                swapImg.alpha = 0.1
+                swapImg.image = self.handleSwipedImagesArray[self.shuffledImagesDict[sender.view!.tag]!]
+                }, completion: { (finished: Bool) -> Void in
+                    UIView.animateWithDuration(0.06, animations: { () -> Void in
+                        swapImg.alpha = 1.0
+                    })
+            })
             
             (handleSwipedImagesArray[shuffledImagesDict[sender.view!.tag]!],handleSwipedImagesArray[shuffledImagesDict[sender.view!.tag - 4]!]) = (handleSwipedImagesArray[shuffledImagesDict[sender.view!.tag - 4]!] , handleSwipedImagesArray[shuffledImagesDict[sender.view!.tag]!])
             
@@ -225,7 +259,14 @@ class PhotoPuzzleViewController: UIViewController, UIGestureRecognizerDelegate {
             img.image = handleSwipedImagesArray[shuffledImagesDict[sender.view!.tag + 4]!]
             
             let swapImg = self.view.viewWithTag(sender.view!.tag + 4) as! UIImageView!
-            swapImg.image = handleSwipedImagesArray[shuffledImagesDict[sender.view!.tag]!]
+            UIView.animateWithDuration(0.6, delay: 0, options: .CurveEaseOut, animations: {
+                swapImg.alpha = 0.1
+                swapImg.image = self.handleSwipedImagesArray[self.shuffledImagesDict[sender.view!.tag]!]
+                }, completion: { (finished: Bool) -> Void in
+                    UIView.animateWithDuration(0.06, animations: { () -> Void in
+                        swapImg.alpha = 1.0
+                    })
+            })
             
             (handleSwipedImagesArray[shuffledImagesDict[sender.view!.tag]!],handleSwipedImagesArray[shuffledImagesDict[sender.view!.tag + 4]!]) = (handleSwipedImagesArray[shuffledImagesDict[sender.view!.tag + 4]!] , handleSwipedImagesArray[shuffledImagesDict[sender.view!.tag]!])
             
@@ -256,10 +297,9 @@ class PhotoPuzzleViewController: UIViewController, UIGestureRecognizerDelegate {
         croppedImagesArray = []
         shuffledImagesDict = [:]
         handleSwipedImagesArray = []
-        imageNameLabel.text = ""
+        //imageNameLabel.text = ""
         viewDidLoad()
     }
-    
 }
 
 //let imageURL = NSURL(fileURLWithPath: "/Users/ramyathay/Desktop/ironManFlying.jpg")
